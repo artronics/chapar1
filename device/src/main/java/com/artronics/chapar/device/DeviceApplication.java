8dce844df0ca522c7d3710cfcbebd12991befad0
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-20 21:06:30 +0100

    add main app

---
+++ device/src/main/java/com/artronics/chapar/device/DeviceApplication.java
@@ -0,0 +1,11 @@
+package com.artronics.chapar.device;
+
+import org.apache.log4j.Logger;
+
+public class DeviceApplication {
+    private final static Logger log = Logger.getLogger(DeviceApplication.class);
+
+    public static void main(String[] args){
+
+    }
+}
