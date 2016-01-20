58ef5db2f392a7b67010979c6ad27edd1323b510
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-20 23:06:08 +0100

    add session

--- device/src/main/java/com/artronics/chapar/device/connection/ControllerResolverImpl.java
+++ device/src/main/java/com/artronics/chapar/device/connection/ControllerResolverImpl.java
@@ -1,6 +1,8 @@
 package com.artronics.chapar.device.connection;
 
+import com.artronics.chapar.core.entities.Session;
 import com.artronics.chapar.core.remote.RemoteControllerService;
+import com.artronics.chapar.core.remote.SessionManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,15 +14,25 @@ public class ControllerResolverImpl implements ControllerResolver{
 
     private RemoteControllerService remoteControllerService;
 
+    private SessionManager sessionManager;
+
     @Override
     public void connect(String url) {
         log.debug("Connecting to controller");
-        remoteControllerService.connect(url);
+        Session session=remoteControllerService.connect(url);
+        log.debug("Set current session: " + session);
+        sessionManager.setSession(session);
     }
 
     @Autowired
     @Qualifier("hessianRemoteControllerService")
     public void setRemoteControllerService(RemoteControllerService remoteControllerService) {
         this.remoteControllerService = remoteControllerService;
+    }
+
+    @Autowired
+    @Qualifier("deviceSessionManager")
+    public void setSessionManager(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
     }
 }
