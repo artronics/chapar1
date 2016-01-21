2684e87845637fc87d663066f525d4e8c87714fe
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-21 19:00:30 +0100

    add packetController with post method and test

---
+++ controller/src/main/java/com/artronics/chapar/controller/http/controllers/PacketController.java
@@ -0,0 +1,20 @@
+package com.artronics.chapar.controller.http.controllers;
+
+import com.artronics.chapar.core.entities.Buffer;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/device/{id}/packet")
+public class PacketController {
+    private final static Logger log = Logger.getLogger(PacketController.class);
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void receivePacket(@PathVariable Long id, @RequestBody Buffer buffer){
+        log.debug(buffer);
+    }
+}
---
+++ controller/src/test/java/com/artronics/chapar/controller/http/controllers/PacketControllerTest.java
@@ -0,0 +1,46 @@
+package com.artronics.chapar.controller.http.controllers;
+
+import com.artronics.chapar.core.entities.Buffer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class PacketControllerTest {
+
+    @InjectMocks
+    PacketController packetController;
+
+    MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(packetController).build();
+    }
+
+    @Test
+    public void it_should_receive_a_json_of_Buffer() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Buffer buffer = new Buffer(Arrays.asList(1, 2, 3));
+        String buff = mapper.writeValueAsString(buffer);
+
+        mockMvc.perform(post("/device/1/packet")
+                .contentType(MediaType.APPLICATION_JSON)
+        .content(buff))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
