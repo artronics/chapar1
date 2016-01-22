76324109c30a45b23fa7809df6704963bc8f8ab3
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-22 06:07:38 +0100

    add resource config and device registration controller

---
+++ controller/src/main/java/com/artronics/chapar/controller/config/ControllerResourceConfig.java
@@ -0,0 +1,10 @@
+package com.artronics.chapar.controller.config;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ControllerResourceConfig {
+    private final static Logger log = Logger.getLogger(ControllerResourceConfig.class);
+
+}
--- controller/src/main/java/com/artronics/chapar/controller/http/controllers/DeviceRegistrationController.java
+++ controller/src/main/java/com/artronics/chapar/controller/http/controllers/DeviceRegistrationController.java
@@ -4,6 +4,7 @@ import com.artronics.chapar.core.entities.Device;
 import com.artronics.chapar.core.repositories.DeviceRepo;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class DeviceRegistrationController {
     DeviceRepo deviceRepo;
 
     @RequestMapping(method = RequestMethod.GET,produces = "application/json")
-    public Device registerDevice(){
+    public Device registerDevice(@PathVariable Long sinkAddress){
         return deviceRepo.save(new Device());
     }
 
