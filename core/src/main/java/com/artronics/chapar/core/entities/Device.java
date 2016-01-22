68413f2b26b425b25ea107c224cd59dbcebe59ad
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-22 07:52:55 +0100

    override hash and equal for device

--- core/src/main/java/com/artronics/chapar/core/entities/Device.java
+++ core/src/main/java/com/artronics/chapar/core/entities/Device.java
@@ -1,5 +1,7 @@
 package com.artronics.chapar.core.entities;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.log4j.Logger;
 
 import javax.persistence.Entity;
@@ -10,4 +12,32 @@ import javax.persistence.Table;
 public class Device extends AbstractBaseEntity{
     private final static Logger log = Logger.getLogger(Device.class);
 
+    @Override
+    public int hashCode()
+    {
+        //use getters for getting fields(for ORM) see this SO answer:
+        //http://stackoverflow.com/questions/27581/what-issues-should-be-considered-when
+        // -overriding-equals-and-hashcode-in-java
+
+        HashCodeBuilder hcb = new HashCodeBuilder();
+        hcb.append(this.id);
+
+        return hcb.toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof Device))
+            return false;
+        if (obj == this)
+            return true;
+
+        Device that = (Device) obj;
+        EqualsBuilder eb = new EqualsBuilder();
+
+        eb.append(this.id,that.id);
+
+        return eb.isEquals();
+    }
 }
