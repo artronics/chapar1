5f5fed3445ec9ca03cfbf09b2cf5d57eccfb69d6
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-22 08:00:58 +0100

    add test for creating map

--- controller/src/main/java/com/artronics/chapar/controller/services/impl/DeviceRegistrationServiceImpl.java
+++ controller/src/main/java/com/artronics/chapar/controller/services/impl/DeviceRegistrationServiceImpl.java
@@ -2,23 +2,44 @@ package com.artronics.chapar.controller.services.impl;
 
 import com.artronics.chapar.controller.services.DeviceRegistrationService;
 import com.artronics.chapar.core.entities.Device;
+import com.artronics.chapar.core.map.DeviceMap;
+import com.artronics.chapar.core.map.DeviceMapImpl;
 import com.artronics.chapar.core.repositories.DeviceRepo;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.Map;
+
 @Service
 public class DeviceRegistrationServiceImpl implements DeviceRegistrationService{
     private final static Logger log = Logger.getLogger(DeviceRegistrationServiceImpl.class);
 
-    @Autowired
     private DeviceRepo deviceRepo;
+
+    private Map<Device,DeviceMap> registeredDevices;
 
     @Override
     public Device registerDevice(Device device) {
         log.debug("Registering new device: "+device);
+        DeviceMap deviceMap = new DeviceMapImpl();
+
         deviceRepo.save(device);
+
+        registeredDevices.put(device,deviceMap);
 
         return device;
     }
+
+    @Resource(name = "registeredDevices")
+    public void setRegisteredDevices(Map<Device, DeviceMap> registeredDevices) {
+        this.registeredDevices = registeredDevices;
+    }
+
+    @Autowired
+    public void setDeviceRepo(DeviceRepo deviceRepo) {
+        this.deviceRepo = deviceRepo;
+    }
+
 }
--- controller/src/test/java/com/artronics/chapar/controller/services/DeviceRegistrationServiceTest.java
+++ controller/src/test/java/com/artronics/chapar/controller/services/DeviceRegistrationServiceTest.java
@@ -11,10 +11,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.HashMap;
 import java.util.Map;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 
 public class DeviceRegistrationServiceTest extends BaseControllerTest{
@@ -25,8 +30,6 @@ public class DeviceRegistrationServiceTest extends BaseControllerTest{
     @Mock
     private DeviceRepo deviceRepo;
 
-//    @Resource("registeredDevices")
-    @Mock
     private Map<Device,DeviceMap> registeredDevices;
 
     private Device device;
@@ -34,14 +37,37 @@ public class DeviceRegistrationServiceTest extends BaseControllerTest{
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+
+        registeredDevices = new HashMap<>();
+        registrationService.setRegisteredDevices(registeredDevices);
+
         device = new Device();
         device.setId(1L);
-//        when(deviceRepo.save())
+        when(deviceRepo.save(device)).thenReturn(device);
     }
+
     @Test
     public void it_should_add_registered_device_in_registeredDevices(){
-
+        registrationService.registerDevice(device);
+        assertTrue(registeredDevices.containsKey(device));
     }
+
+    @Test
+    public void it_should_create_new_DeviceMap_for_each_device(){
+        Device device2 = new Device();
+        device2.setId(2L);
+        when(deviceRepo.save(device2)).thenReturn(device2);
+
+        registrationService.registerDevice(device);
+        registrationService.registerDevice(device2);
+
+        assertThat(registeredDevices.size(),equalTo(2));
+
+        DeviceMap map1 = registeredDevices.get(device);
+        DeviceMap map2 = registeredDevices.get(device2);
+        assertThat(map1,is(not(map2)));
+    }
+
     @Test
     public void it_should_persist_device(){
         registrationService.registerDevice(device);
