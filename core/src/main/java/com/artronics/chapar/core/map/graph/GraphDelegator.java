1849b8c5a32fefa4dbbae2451314885699227a44
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-22 04:45:48 +0100

    add graphDelegator this is not generic any more

---
+++ core/src/main/java/com/artronics/chapar/core/map/graph/GraphDelegator.java
@@ -0,0 +1,15 @@
+package com.artronics.chapar.core.map.graph;
+
+import com.artronics.chapar.core.entities.Node;
+
+import java.util.List;
+import java.util.Set;
+
+public interface GraphDelegator
+{
+    List<Node> getShortestPath(Node source, Node target);
+
+    Set<Node> getNeighbors(Node node);
+
+    boolean isIsland(Node neighbor);
+}
