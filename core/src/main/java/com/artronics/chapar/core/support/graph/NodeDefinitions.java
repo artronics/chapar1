3411b93d25d313946d5306888b876b275adc4526
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-22 04:45:09 +0100

    create a suport class for creating sample graphs mainly for tests

---
+++ core/src/main/java/com/artronics/chapar/core/support/graph/NodeDefinitions.java
@@ -0,0 +1,90 @@
+package com.artronics.chapar.core.support.graph;
+
+import com.artronics.chapar.core.entities.Node;
+
+public class NodeDefinitions {
+    protected Node sink1 = new Node(0L);
+    protected Node sink2 = new Node(1L);
+
+    protected Node sameAddNode1 = new Node(30L);
+    protected Node sameAddNode2 = new Node(30L);
+
+    protected Node node135 = new Node(135L);
+    protected Node node136 = new Node(136L);
+    protected Node node137 = new Node(137L);
+
+    protected Node node245 = new Node(245L);
+    protected Node node246 = new Node(246L);
+
+    public Node getSink1() {
+        return sink1;
+    }
+
+    public void setSink1(Node sink1) {
+        this.sink1 = sink1;
+    }
+
+    public Node getSink2() {
+        return sink2;
+    }
+
+    public void setSink2(Node sink2) {
+        this.sink2 = sink2;
+    }
+
+    public Node getSameAddNode1() {
+        return sameAddNode1;
+    }
+
+    public void setSameAddNode1(Node sameAddNode1) {
+        this.sameAddNode1 = sameAddNode1;
+    }
+
+    public Node getSameAddNode2() {
+        return sameAddNode2;
+    }
+
+    public void setSameAddNode2(Node sameAddNode2) {
+        this.sameAddNode2 = sameAddNode2;
+    }
+
+    public Node getNode135() {
+        return node135;
+    }
+
+    public void setNode135(Node node135) {
+        this.node135 = node135;
+    }
+
+    public Node getNode136() {
+        return node136;
+    }
+
+    public void setNode136(Node node136) {
+        this.node136 = node136;
+    }
+
+    public Node getNode137() {
+        return node137;
+    }
+
+    public void setNode137(Node node137) {
+        this.node137 = node137;
+    }
+
+    public Node getNode245() {
+        return node245;
+    }
+
+    public void setNode245(Node node245) {
+        this.node245 = node245;
+    }
+
+    public Node getNode246() {
+        return node246;
+    }
+
+    public void setNode246(Node node246) {
+        this.node246 = node246;
+    }
+}
