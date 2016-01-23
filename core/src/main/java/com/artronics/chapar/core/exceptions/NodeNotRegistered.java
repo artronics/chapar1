1ad95ee1b57964f48e3d1353f06575081b54c891
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-23 16:50:12 +0100

    add exp

---
+++ core/src/main/java/com/artronics/chapar/core/exceptions/NodeNotRegistered.java
@@ -0,0 +1,7 @@
+package com.artronics.chapar.core.exceptions;
+
+import org.apache.log4j.Logger;
+
+public class NodeNotRegistered extends Exception{
+    private final static Logger log = Logger.getLogger(NodeNotRegistered.class);
+}
