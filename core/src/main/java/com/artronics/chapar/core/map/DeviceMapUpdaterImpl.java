71d693b2d1e8626d32b0982f492716cbbe96206e
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-23 23:14:23 +0100

    add logic to method

--- core/src/main/java/com/artronics/chapar/core/map/DeviceMapUpdaterImpl.java
+++ core/src/main/java/com/artronics/chapar/core/map/DeviceMapUpdaterImpl.java
@@ -3,8 +3,10 @@ package com.artronics.chapar.core.map;
 import com.artronics.chapar.core.entities.Link;
 import com.artronics.chapar.core.entities.Node;
 import com.artronics.chapar.core.exceptions.NodeNotRegistered;
+import com.artronics.chapar.core.utils.LinkUtils;
 import org.apache.log4j.Logger;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class DeviceMapUpdaterImpl implements DeviceMapUpdater {
@@ -15,7 +17,22 @@ public class DeviceMapUpdaterImpl implements DeviceMapUpdater {
         checkIfNodesAreRegistered(deviceMap,srcNode,links);
 
         Set<Link> oldLinks = deviceMap.getLinks(srcNode);
+        Set<Link> mergedLinks = LinkUtils.merge(oldLinks,links);
 
+        Set<Link> removedLinks = new HashSet<>(oldLinks);
+        removedLinks.remove(links);
+
+        mergedLinks.forEach(link -> {
+            Node dstNode = link.getDstNode();
+            if (removedLinks.contains(link)){
+                deviceMap.removeLink(srcNode, dstNode);
+                if (deviceMap.isIsland(dstNode))
+                    islandNodes.add(dstNode);
+            }
+            else {
+                deviceMap.addLink(srcNode, dstNode,link.getWeight());
+            }
+        });
     }
 
     private static void checkIfNodesAreRegistered(DeviceMap deviceMap, Node srcNode, Set<Link> links) throws NodeNotRegistered {
