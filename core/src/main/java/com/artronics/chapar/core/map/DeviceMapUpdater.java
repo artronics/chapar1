28b45b6580beab519c4b33187ca0f3747b0d0230
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-23 16:50:33 +0100

    add a method to check if all nodes are registered before updating map. otherwise it throws exp.s

--- core/src/main/java/com/artronics/chapar/core/map/DeviceMapUpdater.java
+++ core/src/main/java/com/artronics/chapar/core/map/DeviceMapUpdater.java
@@ -2,9 +2,10 @@ package com.artronics.chapar.core.map;
 
 import com.artronics.chapar.core.entities.Link;
 import com.artronics.chapar.core.entities.Node;
+import com.artronics.chapar.core.exceptions.NodeNotRegistered;
 
 import java.util.Set;
 
 public interface DeviceMapUpdater {
-    void update(DeviceMap deviceMap, Node srcNode, Set<Link> links);
+    void update(DeviceMap deviceMap, Node srcNode, Set<Link> links) throws NodeNotRegistered;
 }
--- core/src/main/java/com/artronics/chapar/core/map/DeviceMapUpdaterImpl.java
+++ core/src/main/java/com/artronics/chapar/core/map/DeviceMapUpdaterImpl.java
@@ -2,15 +2,26 @@ package com.artronics.chapar.core.map;
 
 import com.artronics.chapar.core.entities.Link;
 import com.artronics.chapar.core.entities.Node;
+import com.artronics.chapar.core.exceptions.NodeNotRegistered;
 import org.apache.log4j.Logger;
 
 import java.util.Set;
 
-public class DeviceMapUpdaterImpl implements DeviceMapUpdater{
+public class DeviceMapUpdaterImpl implements DeviceMapUpdater {
     private final static Logger log = Logger.getLogger(DeviceMapUpdaterImpl.class);
 
     @Override
-    public void update(DeviceMap deviceMap, Node srcNode, Set<Link> links) {
+    public void update(DeviceMap deviceMap, Node srcNode, Set<Link> links) throws NodeNotRegistered {
+        checkIfNodesAreRegistered(deviceMap,srcNode,links);
+    }
+
+    private void checkIfNodesAreRegistered(DeviceMap deviceMap, Node srcNode, Set<Link> links) throws NodeNotRegistered {
+        if (!deviceMap.contains(srcNode))
+            throw new NodeNotRegistered();
+
+        if (links.stream().anyMatch(link -> !deviceMap.contains(link.getDstNode())))
+            throw new NodeNotRegistered();
 
     }
+
 }
