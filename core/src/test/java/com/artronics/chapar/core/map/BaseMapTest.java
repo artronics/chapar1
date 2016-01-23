ba8550ce6e1226652f01bab3b82d71246fb87560
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-23 21:41:08 +0100

    add graph image to base test for ref

--- core/src/test/java/com/artronics/chapar/core/map/BaseMapTest.java
+++ core/src/test/java/com/artronics/chapar/core/map/BaseMapTest.java
@@ -5,6 +5,26 @@ import org.junit.Before;
 public class BaseMapTest extends BaseGraphTest{
     protected DeviceMap deviceMap;
 
+    /*
+     * node number 30 is sameAddNode
+     *
+     * Graph is like
+     *       sink:0
+     *       /   \
+     *      w50  w10
+     *      /      \
+     *   135 --w20-- 30
+     *     \         /
+     *     w25    w100
+     *       \    /
+     *        136
+     *         |
+     *        w30
+     *         |
+     *        137
+     *
+     *
+     */
     @Override
     @Before
     public void setUp() throws Exception {
