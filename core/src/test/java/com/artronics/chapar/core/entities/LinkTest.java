0fef4fb3837afad632b15b61736024e9707866d1
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-23 17:27:02 +0100

    add tests for equality and hash

--- core/src/test/java/com/artronics/chapar/core/entities/LinkTest.java
+++ core/src/test/java/com/artronics/chapar/core/entities/LinkTest.java
@@ -2,8 +2,10 @@ package com.artronics.chapar.core.entities;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class LinkTest {
@@ -21,6 +23,39 @@ public class LinkTest {
         assertThat(aLink,is(equalTo(aLink)));
 
         assertThat(aLink,is(equalTo(sameLink)));
+        assertThat(sameLink,is(equalTo(aLink)));
+
+        assertThat(aLink,is(not(equalTo(otherLink))));
+    }
+
+    @Test
+    public void for_equality_weight_is_not_important(){
+        Link sameLinkWithDiffWeight = new Link(aNode,233D);
+
+        assertThat(aLink,is(equalTo(sameLinkWithDiffWeight)));
+
+        assertThat(sameLinkWithDiffWeight,is(equalTo(aLink)));
+    }
+
+    @Test
+    public void if_two_links_are_equal_hash_must_be_equal(){
+        assertThat(aLink.hashCode(),is(equalTo(sameLink.hashCode())));
+    }
+
+    @Test
+    public void test_in_HashSet(){
+        Set<Link> links = new HashSet<>();
+        links.add(aLink);
+        links.add(sameLink);
+
+        assertThat(links.size(),is(equalTo(1)));
+
+        Link sameLinkWithDiffWeight = new Link(aNode,233D);
+        links.add(sameLinkWithDiffWeight);
+        assertThat(links.size(),is(equalTo(1)));
+
+        links.add(otherLink);
+        assertThat(links.size(),is(equalTo(2)));
     }
 
 }
