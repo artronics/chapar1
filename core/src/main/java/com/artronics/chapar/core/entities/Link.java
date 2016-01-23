9b58204eec3fd2f05957a1aa12b379ef8b3feffc
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-23 17:19:23 +0100

    add equal and hash builder

--- core/src/main/java/com/artronics/chapar/core/entities/Link.java
+++ core/src/main/java/com/artronics/chapar/core/entities/Link.java
@@ -1,5 +1,8 @@
 package com.artronics.chapar.core.entities;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Link {
 
     private Node dstNode;
@@ -18,4 +21,30 @@ public class Link {
     public Double getWeight() {
         return weight;
     }
+
+    @Override
+    public int hashCode()
+    {
+        HashCodeBuilder hcb = new HashCodeBuilder();
+        hcb.append(this.dstNode);
+
+        return hcb.toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (!(obj instanceof Link))
+            return false;
+        if (obj == this)
+            return true;
+
+        Link that = (Link) obj;
+        EqualsBuilder eb = new EqualsBuilder();
+
+        eb.append(this.dstNode,that.dstNode);
+
+        return eb.isEquals();
+    }
+
 }
--- core/src/test/java/com/artronics/chapar/core/entities/LinkTest.java
+++ core/src/test/java/com/artronics/chapar/core/entities/LinkTest.java
@@ -1,5 +1,26 @@
 package com.artronics.chapar.core.entities;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class LinkTest {
+
+    Node aNode = new Node(10L);
+    Node sameNode = new Node(10L);
+    Node otherNode = new Node(11L);
+
+    Link aLink = new Link(aNode,10D);
+    Link sameLink = new Link(sameNode,123D);
+    Link otherLink = new Link(otherNode,233D);
+
+    @Test
+    public void equal_general_test(){
+        assertThat(aLink,is(equalTo(aLink)));
+
+        assertThat(aLink,is(equalTo(sameLink)));
+    }
 
 }
