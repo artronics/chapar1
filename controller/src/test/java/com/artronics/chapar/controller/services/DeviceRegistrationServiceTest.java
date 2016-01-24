5438b375fe29903cfd073c9fce5865238d625b6a
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-24 23:31:12 +0100

    delete test related to deviceMap for each device

--- controller/src/test/java/com/artronics/chapar/controller/services/DeviceRegistrationServiceTest.java
+++ controller/src/test/java/com/artronics/chapar/controller/services/DeviceRegistrationServiceTest.java
@@ -2,16 +2,12 @@ package com.artronics.chapar.controller.services;
 
 import com.artronics.chapar.controller.services.impl.BaseServiceTest;
 import com.artronics.chapar.controller.services.impl.DeviceRegistrationServiceImpl;
-import com.artronics.chapar.core.entities.Device;
 import com.artronics.chapar.core.repositories.DeviceRepo;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -37,23 +33,6 @@ public class DeviceRegistrationServiceTest extends BaseServiceTest{
     public void it_should_add_registered_device_in_registeredDevices(){
         registrationService.registerDevice(device);
         assertTrue(registeredDevices.containsKey(device.getId()));
-    }
-
-    @Ignore("refactor")
-    @Test
-    public void it_should_create_new_DeviceMap_for_each_device(){
-        Device device2 = new Device();
-        device2.setId(2L);
-        when(deviceRepo.save(device2)).thenReturn(device2);
-
-        registrationService.registerDevice(device);
-        registrationService.registerDevice(device2);
-
-        assertThat(registeredDevices.size(),equalTo(2));
-
-//        DeviceMap map1 = registeredDevices.get(device);
-//        DeviceMap map2 = registeredDevices.get(device2);
-//        assertThat(map1,is(not(map2)));
     }
 
     @Test
