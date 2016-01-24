f16575040e6ede92644c38dfef80f2bfc2afeb10
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-24 14:01:10 +0100

    add beffer servicea

--- controller/src/main/java/com/artronics/chapar/controller/http/controllers/BufferController.java
+++ controller/src/main/java/com/artronics/chapar/controller/http/controllers/BufferController.java
@@ -1,7 +1,10 @@
 package com.artronics.chapar.controller.http.controllers;
 
+import com.artronics.chapar.controller.services.BufferService;
 import com.artronics.chapar.core.entities.Buffer;
+import com.artronics.chapar.core.exceptions.MalformedPacketException;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,8 +12,23 @@ import org.springframework.web.bind.annotation.*;
 public class BufferController {
     private final static Logger log = Logger.getLogger(BufferController.class);
 
+    private BufferService bufferService;
+
     @RequestMapping(value = "/buffer",method = {RequestMethod.POST,RequestMethod.GET})
-    public void receivePacket(@PathVariable int id, @RequestBody Buffer buffer){
+    public void receivePacket(@PathVariable Long id, @RequestBody Buffer buffer){
+        try {
+            bufferService.addBuffer(id,buffer);
+
+
+        } catch (MalformedPacketException e) {
+            e.printStackTrace();
+        }
         log.debug(buffer);
     }
+
+    @Autowired
+    public void setBufferService(BufferService bufferService) {
+        this.bufferService = bufferService;
+    }
+
 }
