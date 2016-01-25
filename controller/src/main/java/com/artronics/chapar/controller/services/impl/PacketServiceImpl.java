1a92283d7dc6823ea5271a5bc014f997600db749
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-01-25 01:30:40 +0100

    inject node and address registration services to packet service

--- controller/src/main/java/com/artronics/chapar/controller/services/impl/PacketServiceImpl.java
+++ controller/src/main/java/com/artronics/chapar/controller/services/impl/PacketServiceImpl.java
@@ -1,7 +1,12 @@
 package com.artronics.chapar.controller.services.impl;
 
 import com.artronics.chapar.controller.factories.PacketFactory;
+import com.artronics.chapar.controller.services.AddressRegistrationService;
+import com.artronics.chapar.controller.services.NodeRegistrationService;
 import com.artronics.chapar.controller.services.PacketService;
+import com.artronics.chapar.core.entities.Address;
+import com.artronics.chapar.core.entities.Device;
+import com.artronics.chapar.core.entities.Node;
 import com.artronics.chapar.core.entities.Packet;
 import com.artronics.chapar.core.exceptions.MalformedPacketException;
 import org.apache.log4j.Logger;
@@ -14,9 +19,32 @@ public class PacketServiceImpl extends BaseService implements PacketService{
 
     private PacketFactory packetFactory;
 
+    private AddressRegistrationService addressRegistrationService;
+
+    private NodeRegistrationService nodeRegistrationService;
+
     @Override
     public void addPacket(Packet packet,Long deviceId) throws MalformedPacketException {
         packet = packetFactory.create(packet);
+
+        Device device = registeredDevices.get(deviceId);
+
+        Address srcAddress = packet.getSrcAddress();
+        srcAddress.setDevice(device);
+
+        Address dstAddress = packet.getDstAddress();
+        dstAddress.setDevice(device);
+
+        addressRegistrationService.resolveAddress(srcAddress,dstAddress);
+
+        Node srcNode = Node.create(srcAddress);
+        Node dstNode = Node.create(dstAddress);
+        nodeRegistrationService.registerNode(srcNode,dstNode);
+    }
+
+    @Autowired
+    public void setAddressRegistrationService(AddressRegistrationService addressRegistrationService) {
+        this.addressRegistrationService = addressRegistrationService;
     }
 
     @Autowired
