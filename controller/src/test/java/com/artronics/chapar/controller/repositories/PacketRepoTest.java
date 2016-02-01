a47c52d0a4e3ee88f301de3800d48b17c5faaeaf
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-02-01 08:16:02 +0100

    add a test to PacketRepo. save a packet with all src dst addresses and buffer

--- controller/src/test/java/com/artronics/chapar/controller/repositories/PacketRepoTest.java
+++ controller/src/test/java/com/artronics/chapar/controller/repositories/PacketRepoTest.java
@@ -1,8 +1,16 @@
 package com.artronics.chapar.controller.repositories;
 
 import com.artronics.chapar.controller.BaseControllerTestConfig;
+import com.artronics.chapar.controller.entities.packet.Packet;
 import com.artronics.chapar.controller.entities.packet.SdwnPacket;
 import com.artronics.chapar.controller.sdwn.packet.SdwnPacketType;
+import com.artronics.chapar.domain.entities.Buffer;
+import com.artronics.chapar.domain.entities.Client;
+import com.artronics.chapar.domain.entities.address.Address;
+import com.artronics.chapar.domain.entities.address.UnicastAddress;
+import com.artronics.chapar.domain.repositories.AddressRepo;
+import com.artronics.chapar.domain.repositories.BufferRepo;
+import com.artronics.chapar.domain.repositories.ClientRepo;
 import com.artronics.chapar.domain.repositories.PersistenceConfig;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +24,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Arrays;
+
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
@@ -26,17 +36,50 @@ import static org.junit.Assert.assertThat;
 public class PacketRepoTest {
     @Autowired
     private PacketRepo packetRepo;
+    @Autowired
+    private AddressRepo addressRepo;
+    @Autowired
+    private ClientRepo clientRepo;
+    @Autowired
+    private BufferRepo bufferRepo;
+
+    private Client client;
+    private UnicastAddress srcAdd;
+    private Address dstAdd;
+    private Buffer buffer;
 
     @Before
     public void setUp() throws Exception {
+        client = new Client();
+        clientRepo.save(client);
 
+        srcAdd = UnicastAddress.create(client,1000L);
+        dstAdd = UnicastAddress.create(client,2000L);
 
+        addressRepo.save(srcAdd);
+        addressRepo.save(dstAdd);
+
+        buffer = new Buffer(Arrays.asList(1,2,3,4,5,6,2,5,4,3,2,3,45,6), Buffer.Direction.TX);
+        bufferRepo.save(buffer);
     }
 
     @Test
     public void it_should_save_a_packet() throws Exception {
         SdwnPacket p = new SdwnPacket();
         p.setType(SdwnPacketType.REPORT);
+        packetRepo.save(p);
+
+        assertThat(p.getId(),is(notNullValue()));
+    }
+
+    @Test
+    public void it_should_save_packet_with_addresses() throws Exception {
+        Packet p = new SdwnPacket();
+        p.setType(SdwnPacketType.REPORT);
+        p.setBuffer(buffer);
+        p.setSrcAddress(srcAdd);
+        p.setDstAddress(dstAdd);
+
         packetRepo.save(p);
 
         assertThat(p.getId(),is(notNullValue()));
