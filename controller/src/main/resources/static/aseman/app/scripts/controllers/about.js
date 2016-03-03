ab438f16fa55f2763c2c3334db2d29bf5a24e7fb
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-03-03 16:35:11 +0100

    add static web front end. Initial yo template

---
+++ controller/src/main/resources/static/aseman/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name asemanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the asemanApp
+ */
+angular.module('asemanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ controller/src/main/resources/static/aseman/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name asemanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the asemanApp
+ */
+angular.module('asemanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
